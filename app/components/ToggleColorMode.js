41b75727b11b36af259c65881d5e30801e8daa6f
Anvesh <atvanvesh@gmail.com>
2024-08-16 10:04:51 -0500

    added changes to ui and removed tsx

--- app/components/ToggleColorMode.js
+++ app/components/ToggleColorMode.js
@@ -1,23 +1,25 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
+"use client";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import PropTypes from "prop-types";
 
-import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
-import ModeNightRoundedIcon from '@mui/icons-material/ModeNightRounded';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import WbSunnyRoundedIcon from "@mui/icons-material/WbSunnyRounded";
+import ModeNightRoundedIcon from "@mui/icons-material/ModeNightRounded";
 
 function ToggleColorMode({ mode, toggleColorMode }) {
   return (
-    <Box sx={{ maxWidth: '32px' }}>
+    <Box sx={{ maxWidth: "32px" }}>
       <Button
         variant="text"
         onClick={toggleColorMode}
         size="small"
         aria-label="button to toggle theme"
-        sx={{ minWidth: '32px', height: '32px', p: '4px' }}
+        sx={{ minWidth: "32px", height: "32px", p: "4px" }}
       >
-        {mode === 'dark' ? (
+        {mode === "dark" ? (
           <WbSunnyRoundedIcon fontSize="small" />
         ) : (
           <ModeNightRoundedIcon fontSize="small" />
@@ -28,7 +30,7 @@ function ToggleColorMode({ mode, toggleColorMode }) {
 }
 
 ToggleColorMode.propTypes = {
-  mode: PropTypes.oneOf(['dark', 'light']).isRequired,
+  mode: PropTypes.oneOf(["dark", "light"]).isRequired,
   toggleColorMode: PropTypes.func.isRequired,
 };
 
